da12058d1e60727403cc5b8e4b39d9334bf51ed6
SkripalBoris <skipalboris@gmail.com>
2015-10-02 01:36:58 +0300

    Исправлены ошибки сборки

--- app/src/main/java/app/travelalarmclock/grafinterface/geoalarm/geoalarm/newzad.java
+++ app/src/main/java/app/travelalarmclock/grafinterface/geoalarm/geoalarm/newzad.java
@@ -1,11 +1,13 @@
 package app.travelalarmclock.grafinterface.geoalarm.geoalarm;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class newzad extends AppCompatActivity {
+import app.travelalarmclock.R;
+
+public class newzad extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/app/travelalarmclock/grafinterface/geoalarm/geoalarm/zadania_activity.java
+++ app/src/main/java/app/travelalarmclock/grafinterface/geoalarm/geoalarm/zadania_activity.java
@@ -1,11 +1,13 @@
 package app.travelalarmclock.grafinterface.geoalarm.geoalarm;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class zadania_activity extends AppCompatActivity {
+import app.travelalarmclock.R;
+
+public class zadania_activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
